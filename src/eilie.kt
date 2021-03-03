22a8b9bcc07579a7343df7ab4f4e238842bd8a4a
Anyijukire <anyijukirjanett@gmail.com>
2021-03-03 14:13:47 +0300

    first commit

---
+++ src/eilie.kt
@@ -0,0 +1,49 @@
+fun main() {
+     add()
+    subtract()
+    name()
+    multiplication()
+    modulus()
+    division()
+    compare()
+}
+fun add(){
+    var a=33
+    var b=78
+    var c=45
+    var sum=a+b+c
+    println(sum)
+}
+fun subtract(){
+    var a=45
+    var b=21
+    var subtract=a-b
+    println(subtract)
+}
+fun name(){
+    val nam=("Anyijukire janet")
+    println(nam)
+}
+fun multiplication(){
+    var t=23
+    var u=9
+    var multiply=t*u
+    println(multiply)
+}
+fun modulus(){
+    var a=23
+    var b=3
+    var modulus=a%b
+    println(modulus)
+}
+fun division(){
+    var a=80
+    var b=20
+    var product=a/b
+    println(product)
+}
+fun compare(){
+    var e=56
+    var y=345
+    println(e>y)
+}
